0d7c4be2f83251a2d1f033faef46e88021731867
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2024-04-30 16:07:57 +0200

    fix tests in greenhouse-management

--- ui/greenhouse-management/jest.config.js
+++ ui/greenhouse-management/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!(juno-ui-components|url-state-router|communicator|oauth|url-state-provider|messages-provider|policy-engine)/)",
+    "node_modules/(?!(juno-ui-components|url-state-router|communicator|oauth|url-state-provider|messages-provider|policy-engine|utils)/)",
   ],
   moduleNameMapper: {
     // Jest currently doesn't support resources with query parameters.
