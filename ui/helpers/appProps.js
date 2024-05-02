b3dbe895604f6ee67208aae406ad5b760ca584b5
I574614 <uwe.mayer01@sap.com>
2024-05-02 10:57:29 +0200

    remove duplicate license header

--- ui/helpers/appProps.js
+++ ui/helpers/appProps.js
@@ -3,10 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
 
 const path = require("path")
 const fs = require("fs")
