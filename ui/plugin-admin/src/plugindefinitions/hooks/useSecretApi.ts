c8b07cd2bede20e138de2581c57e8530604b538a
I574614 <uwe.mayer01@sap.com>
2024-05-08 07:41:56 +0200

    infer type in secret api

--- ui/plugin-admin/src/plugindefinitions/hooks/useSecretApi.ts
+++ ui/plugin-admin/src/plugindefinitions/hooks/useSecretApi.ts
@@ -2,25 +2,30 @@ import { Secret } from "../../../../types/types"
 import useApi, { ApiResponse } from "./useApi"
 import useNamespace from "./useNamespace"
 
+export type SecretApiResponse  = {
+  ok: boolean,
+  message: string
+  response?: Secret
+}
 
 export const useSecretApi = () => {
   const {get, create,  update, deleteObject } = useApi()
   const { namespace } = useNamespace()
 
-  const getSecret= (secret: Secret): Promise<ApiResponse> => {
-    return get<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret)
+  const getSecret= (secret: Secret): Promise<SecretApiResponse> => {
+    return get<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret) as Promise<SecretApiResponse>
   }
 
-  const createSecret = (secret: Secret): Promise<ApiResponse> => {
-    return create<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret)
+  const createSecret = (secret: Secret): Promise<SecretApiResponse> => {
+    return create<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret) as Promise<SecretApiResponse>
   }
 
-  const updateSecret = (secret: Secret): Promise<ApiResponse> => {
-    return update<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret)
+  const updateSecret = (secret: Secret): Promise<SecretApiResponse> => {
+    return update<Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret) as Promise<SecretApiResponse>
   }
 
-  const deleteSecret = (secret: Secret): Promise<ApiResponse> => {
-    return deleteObject <Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret)
+  const deleteSecret = (secret: Secret): Promise<SecretApiResponse> => {
+    return deleteObject <Secret>(`/api/v1/namespaces/${namespace}/secrets`, secret) as Promise<SecretApiResponse>
   }
 
   
