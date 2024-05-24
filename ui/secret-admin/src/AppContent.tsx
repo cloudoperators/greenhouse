ff99ca9003cd5830c2bc376137f1473316f3ad0d
Uwe Mayer <115539431+uwe-mayer@users.noreply.github.com>
2024-05-24 10:08:04 +0200

    Fix(ui): secrets add missing import for tests (#236)

--- ui/secret-admin/src/AppContent.tsx
+++ ui/secret-admin/src/AppContent.tsx
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-
+import React from "react"
 import { Container } from "juno-ui-components"
 import SecretEdit from "./components/SecretEdit"
 import SecretList from "./components/SecretList"
