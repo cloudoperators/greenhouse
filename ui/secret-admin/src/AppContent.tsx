1e9a59dabba8dfc636b893241e573f4aba1cab34
I574614 <uwe.mayer01@sap.com>
2024-05-24 08:38:07 +0200

    Fix(ui): secrets
    * allow creation without list permissions
    * Do not list helm release secrets

--- ui/secret-admin/src/AppContent.tsx
+++ ui/secret-admin/src/AppContent.tsx
@@ -3,20 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from "react"
-import { Container, Message, Stack } from "juno-ui-components"
-import { useEffect, useState } from "react"
+import { Container } from "juno-ui-components"
 import SecretEdit from "./components/SecretEdit"
 import SecretList from "./components/SecretList"
 
 import WelcomeView from "./components/WelcomeView"
-import useClient from "./hooks/useClient"
-import useNamespace from "./hooks/useNamespace"
 import useStore from "./store"
-import ResultMessageComponent, {
-  ResultMessage,
-} from "./components/SubmitResultMessage"
-import useCheckAuthorized from "./hooks/useIsAuthorized"
 
 const AppContent = () => {
   const secrets = useStore((state) => state.secrets)
@@ -26,35 +18,11 @@ const AppContent = () => {
   const authError = auth?.error
   const loggedIn = useStore((state) => state.loggedIn)
 
-  const { namespace } = useNamespace()
-  const { client: client } = useClient()
-  const { canListSecrets } = useCheckAuthorized()
-
-  const [authMessage, setAuthMessage] = useState<ResultMessage>({
-    ok: false,
-    message: "",
-  })
-
-  useEffect(() => {
-    canListSecrets().then((res) => {
-      console.log("canListSecrets", res)
-      setAuthMessage(res)
-    })
-  }, [client, namespace])
-
   return (
     <Container>
-      {authMessage.message && (
-        <Stack>
-          <ResultMessageComponent
-            submitMessage={authMessage}
-          ></ResultMessageComponent>
-        </Stack>
-      )}
-
       {loggedIn && !authError ? (
         <>
-          {secrets.length > 0 && <SecretList secrets={secrets} />}
+          <SecretList secrets={secrets} />
           {showSecretEdit && <SecretEdit />}
         </>
       ) : (
