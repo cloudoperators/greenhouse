97252c1b2388b94c806658bf82f49283d0c2b833
Tommy Sauer <github@sauerst.de>
2024-06-26 09:21:09 +0200

    Better description
    Co-authored-by: IvoGoman <ivo.gosemann@sap.com>

--- pkg/apis/well_known.go
+++ pkg/apis/well_known.go
@@ -55,7 +55,7 @@ const (
 	// LabelKeyExposeService is applied to services that are part of a PluginDefinitions Helm chart to expose them via the central Greenhouse infrastructure.
 	LabelKeyExposeService = "greenhouse.sap/expose"
 
-	// LabelKeyExposePortName is specifying the port to be exposed by name. LabelKeyExposeService needs to be set. By default the first port is always exposed.
+	// LabelKeyExposePortName is specifying the port to be exposed by name. LabelKeyExposeService needs to be set. Defaults to the first port if the named port is not found.
 	LabelKeyExposePortName = "greenhouse.sap/exposePortName"
 )
 
