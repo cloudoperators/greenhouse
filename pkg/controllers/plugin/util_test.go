38a74c17ae9cb1706809f15591927cc286374a91
d066607 <tommy.sauer@sap.com>
2024-06-27 15:55:55 +0200

    test(plugin): adding test for named ports exposal

--- pkg/controllers/plugin/util_test.go
+++ pkg/controllers/plugin/util_test.go
@@ -39,4 +39,46 @@ var _ = Describe("validate utility functions", Ordered, func() {
 		Ω(port.Port).
 			Should(Equal(portNumber), "the port should be 80")
 	})
+	It("should get named port from an unstructured service object", func() {
+		var portNumber1 int32 = 80
+		var portNumber2 int32 = 443
+		// Mock an unstructured object with ports
+		unstructuredObj := &unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"apiVersion": "v1",
+				"kind":       "Service",
+				"metadata": map[string]interface{}{
+					"name":      "example-service",
+					"namespace": "default",
+					"labels": map[string]string{
+						"greenhouse.sap/exposeNamedPort": "https",
+					},
+				},
+				"spec": map[string]interface{}{
+					"type": "ClusterIP",
+					"ports": []interface{}{
+						map[string]interface{}{
+							"name":     "http",
+							"port":     portNumber1,
+							"protocol": "TCP",
+						},
+						map[string]interface{}{
+							"name":     "https",
+							"port":     portNumber2,
+							"protocol": "TCP",
+						},
+					},
+				},
+			},
+		}
+		port, err := getPortForExposedService(unstructuredObj)
+		Ω(err).
+			ShouldNot(HaveOccurred(), "there should be no error getting ports from an unstructured service object")
+		Ω(port).
+			ShouldNot(BeNil(), "the port should not be nil")
+		Ω(port).
+			ShouldNot(Equal(portNumber1), "the port should not be 80")
+		Ω(port.Port).
+			Should(Equal(portNumber2), "the port should be 443")
+	})
 })
