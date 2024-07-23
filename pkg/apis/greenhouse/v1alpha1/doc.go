a46206abbd0c1b1d3798dfb9aa332cd194884c62
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2024-07-23 09:17:43 +0200

    removed copy paste comments

--- pkg/apis/greenhouse/v1alpha1/doc.go
+++ pkg/apis/greenhouse/v1alpha1/doc.go
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// Package v1alpha1 contains API Schema definitions for the autoscaling v1alpha1 API group
 // +groupName=greenhouse.sap
 package v1alpha1
