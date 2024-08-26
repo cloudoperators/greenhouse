3fe275e2c896396dbbca6c57882b6013e15ad295
gciezkowski-acc <grzegorz.ciezkowski@accenture.com>
2024-08-26 15:23:04 +0200

    Feat/additional metrics for cluster (#479)
    * feat(cluster) Additional cluster metrics (#453)
    
    * feat(cluster) Additional metrics for cluster in Prometeus
    
    * feat(cluster) Unit tests for reconciler
    
    * feat(cluster) Sending metrics in direct access controller and headscale access controller (#453)
    
    * feat(cluster) Changes after CodeReview (#453)
    
    * Automatic application of license header
    
    ---------
    
    Co-authored-by: License Bot <license_bot@github.com>

---
+++ pkg/controllers/cluster/metrics.go
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package cluster
+
+import (
+	"time"
+
+	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/pkg/apis/greenhouse/v1alpha1"
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+var (
+	kubernetesVersionsGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "greenhouse_cluster_k8s_versions_total",
+		},
+		[]string{"cluster", "namespace", "version"})
+
+	secondsToTokenExpiryGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "greenhouse_cluster_kubeconfig_validity_seconds",
+		},
+		[]string{"cluster", "namespace"})
+)
+
+func init() {
+	metrics.Registry.MustRegister(kubernetesVersionsGauge)
+	metrics.Registry.MustRegister(secondsToTokenExpiryGauge)
+}
+
+func updateMetrics(cluster *greenhousev1alpha1.Cluster) {
+	kubernetesVersionLabels := prometheus.Labels{
+		"cluster":   cluster.Name,
+		"namespace": cluster.Namespace,
+		"version":   cluster.Status.KubernetesVersion,
+	}
+	kubernetesVersionsGauge.With(kubernetesVersionLabels).Set(float64(1))
+
+	secondsToExpiry := cluster.Status.BearerTokenExpirationTimestamp.Unix() - time.Now().Unix()
+	secondsToExpiryLabels := prometheus.Labels{
+		"cluster":   cluster.Name,
+		"namespace": cluster.Namespace,
+	}
+	secondsToTokenExpiryGauge.With(secondsToExpiryLabels).Set(float64(secondsToExpiry))
+}
