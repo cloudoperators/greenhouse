59e53b455207eeb1a2f7944958d49cf75b292ee2
grzegorz-ciezkowski <grzegorz.ciezkowski@accenture.com>
2024-08-26 10:46:57 +0200

    feat(cluster) Sending metrics in direct access controller and headscale access controller (#453)

--- pkg/controllers/cluster/metrics.go
+++ pkg/controllers/cluster/metrics.go
@@ -1,18 +1,12 @@
 package cluster
 
 import (
-	"context"
 	"time"
 
 	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/pkg/apis/greenhouse/v1alpha1"
 	"github.com/prometheus/client_golang/prometheus"
-	"k8s.io/client-go/tools/record"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
-
-const metricsRequeueInterval = 2 * time.Minute
 
 var (
 	kubernetesVersionsCounter = prometheus.NewCounterVec(
@@ -33,31 +27,9 @@ func init() {
 	metrics.Registry.MustRegister(secondsToTokenExpiryGauge)
 }
 
-// ClusterMetricsReconciler reconciles the overall status of a remote cluster
-type ClusterMetricsReconciler struct {
-	client.Client
-	recorder record.EventRecorder
-}
-
 //+kubebuilder:rbac:groups=greenhouse.sap,resources=clusters,verbs=get;list
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *ClusterMetricsReconciler) SetupWithManager(name string, mgr ctrl.Manager) error {
-	r.Client = mgr.GetClient()
-	r.recorder = mgr.GetEventRecorderFor(name)
-
-	return ctrl.NewControllerManagedBy(mgr).
-		Named(name).
-		For(&greenhousev1alpha1.Cluster{}).
-		Complete(r)
-}
-
-func (r *ClusterMetricsReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	var cluster = new(greenhousev1alpha1.Cluster)
-	if err := r.Get(ctx, req.NamespacedName, cluster); err != nil {
-		return ctrl.Result{}, err
-	}
-
+func UpdateMetrics(cluster *greenhousev1alpha1.Cluster) {
 	kubernetesVersionLabels := prometheus.Labels{
 		"cluster":   cluster.Name,
 		"namespace": cluster.Namespace,
@@ -71,6 +43,4 @@ func (r *ClusterMetricsReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		"namespace": cluster.Namespace,
 	}
 	secondsToTokenExpiryGauge.With(secondsToExpiryLabels).Set(float64(secondsToExpiry))
-
-	return ctrl.Result{RequeueAfter: metricsRequeueInterval}, nil
 }
---
+++ pkg/controllers/cluster/metrics_test.go
@@ -0,0 +1,51 @@
+package cluster
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	prometheusTest "github.com/prometheus/client_golang/prometheus/testutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"time"
+
+	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/pkg/apis/greenhouse/v1alpha1"
+	"github.com/cloudoperators/greenhouse/pkg/test"
+)
+
+var _ = Describe("Metrics controller", Ordered, func() {
+	var (
+		//remoteClient     client.Client
+		remoteEnvTest *envtest.Environment
+	)
+
+	BeforeAll(func() {
+		_, _, remoteEnvTest, _ = test.StartControlPlane("6888", false, false)
+	})
+
+	AfterAll(func() {
+		Expect(remoteEnvTest.Stop()).To(Succeed(), "there should be no error stopping the remote environment")
+	})
+
+	It("Should return metrics when reconciler is calling", func() {
+		const clusterName = "test-cluster-a"
+		setup := test.NewTestSetup(test.Ctx, test.K8sClient, "teamrbac")
+		cluster := &greenhousev1alpha1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      clusterName,
+				Namespace: setup.Namespace(),
+			},
+			Status: greenhousev1alpha1.ClusterStatus{
+				KubernetesVersion:              "1.31.1",
+				BearerTokenExpirationTimestamp: metav1.Time{Time: time.Now().Add(600 * time.Second)},
+			},
+		}
+
+		counterBefore := prometheusTest.CollectAndCount(kubernetesVersionsCounter)
+		UpdateMetrics(cluster)
+		counterAfter := prometheusTest.CollectAndCount(kubernetesVersionsCounter)
+		Expect(counterAfter).To(BeEquivalentTo(counterBefore + 1))
+		tokenExpiry := prometheusTest.ToFloat64(secondsToTokenExpiryGauge.WithLabelValues(cluster.Name, cluster.Namespace))
+		Expect(tokenExpiry).To(BeNumerically(">=", 595))
+		Expect(tokenExpiry).To(BeNumerically("<=", 600))
+	})
+})
