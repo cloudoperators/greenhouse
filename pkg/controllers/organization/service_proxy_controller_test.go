07344c334d2808f0bd7aed90693f10871bc257c6
k.zagorski <k.zagorski@accenture.com>
2024-10-15 13:02:37 +0200

    fix: update tests to check for correct plugin creation

--- pkg/controllers/organization/service_proxy_controller_test.go
+++ pkg/controllers/organization/service_proxy_controller_test.go
@@ -15,18 +15,32 @@ import (
 	"github.com/cloudoperators/greenhouse/pkg/test"
 )
 
-var _ = Describe("Organization ServiceProxyReconciler", func() {
-	var setup *test.TestSetup
-
-	BeforeEach(func() {
-		setup = test.NewTestSetup(test.Ctx, test.K8sClient, "org-serviceproxy-test")
-	})
+var _ = Describe("Organization ServiceProxyReconciler", Ordered, func() {
+	var serviceProxyPluginDefinition = &greenhousev1alpha1.PluginDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "PluginDefinition",
+			APIVersion: greenhousev1alpha1.GroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "service-proxy",
+			Namespace: corev1.NamespaceDefault,
+		},
+		Spec: greenhousev1alpha1.PluginDefinitionSpec{
+			Description: "Testplugin",
+			Version:     "1.0.0",
+			HelmChart: &greenhousev1alpha1.HelmChartReference{
+				Name:       "./../../test/fixtures/myChart",
+				Repository: "dummy",
+				Version:    "1.0.0",
+			},
+		},
+	}
 
 	When("plugin definition for service proxy is missing", func() {
-		It("should log about missing plugin definition", func() {
+		It("should log about missing plugin definition and create plugin when it's added", func() {
 			By("ensuring service-proxy plugin definition does not exist")
-			var serviceProxyPluginDefinition = new(greenhousev1alpha1.PluginDefinition)
-			err := setup.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: ""}, serviceProxyPluginDefinition)
+			var pluginDefinition = new(greenhousev1alpha1.PluginDefinition)
+			err := test.K8sClient.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: ""}, pluginDefinition)
 			Expect(err).To(HaveOccurred(), "there should be an error getting the service-proxy plugin definition")
 
 			By("teeing logger to a custom writer")
@@ -35,48 +49,61 @@ var _ = Describe("Organization ServiceProxyReconciler", func() {
 			defer GinkgoWriter.ClearTeeWriters()
 
 			By("creating an organization")
-			setup.CreateOrganization(test.Ctx, "test-serviceproxy-org1")
+			org := &greenhousev1alpha1.Organization{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Organization",
+					APIVersion: greenhousev1alpha1.GroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-serviceproxy-org1",
+				},
+			}
+			err = test.K8sClient.Create(test.Ctx, org)
+			Expect(err).ToNot(HaveOccurred(), "there should be no error creating the organization")
 
 			By("ensuring ServiceProxyController logged about missing plugin definition")
 			Eventually(func() []byte {
 				return tee.Contents()
 			}).Should(ContainSubstring("plugin definition for service-proxy not found"),
 				"ServiceProxyController should log about missing plugin definition")
+
+			By("creating service-proxy plugin definition")
+			err = test.K8sClient.Create(test.Ctx, serviceProxyPluginDefinition)
+			Expect(err).ToNot(HaveOccurred(), "there should be no error creating the service-proxy plugin definition")
+
+			By("ensuring a service-proxy plugin has been created for organization")
+			var plugin = new(greenhousev1alpha1.Plugin)
+			Eventually(func(g Gomega) {
+				err = test.K8sClient.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: org.Name}, plugin)
+				g.Expect(err).ToNot(HaveOccurred(), "service-proxy plugin should have been created by controller")
+			}).Should(Succeed(), "service-proxy plugin should have been created for organization")
 		})
 	})
 
 	When("plugin definition for service proxy is present", func() {
 		It("should create service-proxy plugin for organization", func() {
-			By("creating service-proxy plugin definition")
-			var serviceProxyPluginDefinition = &greenhousev1alpha1.PluginDefinition{
+			By("getting service-proxy plugin definition")
+			var pluginDefinition = new(greenhousev1alpha1.PluginDefinition)
+			err := test.K8sClient.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: ""}, pluginDefinition)
+			Expect(err).ToNot(HaveOccurred(), "there should be no error getting the service-proxy plugin definition")
+
+			By("creating an organization")
+			org := &greenhousev1alpha1.Organization{
 				TypeMeta: metav1.TypeMeta{
-					Kind:       "PluginDefinition",
+					Kind:       "Organization",
 					APIVersion: greenhousev1alpha1.GroupVersion.String(),
 				},
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "service-proxy",
-					Namespace: corev1.NamespaceDefault,
-				},
-				Spec: greenhousev1alpha1.PluginDefinitionSpec{
-					Description: "Testplugin",
-					Version:     "1.0.0",
-					HelmChart: &greenhousev1alpha1.HelmChartReference{
-						Name:       "./../../test/fixtures/myChart",
-						Repository: "dummy",
-						Version:    "1.0.0",
-					},
+					Name: "test-serviceproxy-org2",
 				},
 			}
-			err := setup.Create(test.Ctx, serviceProxyPluginDefinition)
-			Expect(err).ToNot(HaveOccurred(), "there should be no error creating the service-proxy plugin definition")
-
-			By("creating an organization")
-			org := setup.CreateOrganization(test.Ctx, "test-serviceproxy-org2")
+			err = test.K8sClient.Create(test.Ctx, org)
+			Expect(err).ToNot(HaveOccurred(), "there should be no error creating the organization")
 
 			By("ensuring a service-proxy plugin has been created for organization")
 			Eventually(func(g Gomega) {
 				var plugin = new(greenhousev1alpha1.Plugin)
-				err = setup.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: org.Name}, plugin)
+				err = test.K8sClient.Get(test.Ctx, types.NamespacedName{Name: "service-proxy", Namespace: org.Name}, plugin)
 				g.Expect(err).ToNot(HaveOccurred(), "service-proxy plugin should have been created by controller")
 			}).Should(Succeed(), "service-proxy plugin should have been created for organization")
 		})
