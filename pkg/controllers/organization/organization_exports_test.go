565d851079d9f106b6922a8b9a82c67b88cd2341
Uwe Mayer <115539431+uwe-mayer@users.noreply.github.com>
2024-10-23 10:55:33 +0200

    feat(rbac): Introduce further default seeded TeamRoles (#641)
    ---------
    
    Signed-off-by: Uwe Mayer <uwe.mayer01@sap.com>

---
+++ pkg/controllers/organization/organization_exports_test.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package organization
+
+var (
+	ExportDefaultTeamRoles = defaultTeamRoles
+)
