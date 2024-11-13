b5747e8c355435fa2ac9c5eac41e2d600e2072d0
grzegorz-ciezkowski <grzegorz.ciezkowski@accenture.com>
2024-11-13 17:07:32 +0100

    feat(plugin) Main merge side effects (#695)

--- e2e/shared/e2e.go
+++ e2e/shared/e2e.go
@@ -21,7 +21,6 @@ import (
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
