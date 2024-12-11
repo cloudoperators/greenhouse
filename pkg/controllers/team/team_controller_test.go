dd719d1fc761bdfe7f82ab94a58e2b033f99a5df
grzegorz-ciezkowski <grzegorz.ciezkowski@accenture.com>
2024-12-11 16:14:52 +0100

    feat(teams) Unit tests for members in team (#723)

---
+++ pkg/controllers/team/team_controller_test.go
@@ -0,0 +1,68 @@
+package team
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	greenhouseapisv1alpha1 "github.com/cloudoperators/greenhouse/pkg/apis/greenhouse/v1alpha1"
+	"github.com/cloudoperators/greenhouse/pkg/test"
+)
+
+var _ = Describe("TeamControllerTest", Ordered, func() {
+	It("Should update status of team with members", func() {
+		err := test.K8sClient.Create(test.Ctx, &greenhouseapisv1alpha1.Team{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Team",
+				APIVersion: greenhouseapisv1alpha1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-team",
+				Namespace: test.TestNamespace,
+			},
+			Spec: greenhouseapisv1alpha1.TeamSpec{
+				Description:    "",
+				MappedIDPGroup: "MAP_IDP_GROUP",
+			},
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		err = test.K8sClient.Create(test.Ctx, &greenhouseapisv1alpha1.TeamMembership{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "TeamMembership",
+				APIVersion: greenhouseapisv1alpha1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-membership",
+				Namespace: test.TestNamespace,
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: greenhouseapisv1alpha1.GroupVersion.String(),
+						Kind:       "Team",
+						Name:       "test-team",
+						UID:        "uhuihiuh",
+					},
+				},
+			},
+			Spec: greenhouseapisv1alpha1.TeamMembershipSpec{
+				Members: []greenhouseapisv1alpha1.User{
+					{
+						ID:        "d2a72c04-42d2-426a-942d-af9609c4cd00",
+						FirstName: "John",
+						LastName:  "Doe",
+						Email:     "john.doe@example.com",
+					},
+				},
+			},
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func(g Gomega) {
+			team := &greenhouseapisv1alpha1.Team{}
+			err := test.K8sClient.Get(test.Ctx, client.ObjectKey{Name: "test-team", Namespace: test.TestNamespace}, team)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(team.Status.Members).To(HaveLen(1))
+		}).Should(Succeed())
+	})
+})
