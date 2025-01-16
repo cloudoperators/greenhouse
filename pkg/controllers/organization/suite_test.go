7caec01658961f3415870a046f8a2796d2d88226
abhijith-darshan <abhijith.ravindra@sap.com>
2025-01-16 16:27:13 +0100

    (chore): spin up postgres test container for organization test

--- pkg/controllers/organization/suite_test.go
+++ pkg/controllers/organization/suite_test.go
@@ -4,6 +4,10 @@
 package organization_test
 
 import (
+	"context"
+	"github.com/dexidp/dex/storage/sql"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"net/http/httptest"
 	"testing"
 
@@ -14,10 +18,20 @@ import (
 	organizationpkg "github.com/cloudoperators/greenhouse/pkg/controllers/organization"
 	"github.com/cloudoperators/greenhouse/pkg/scim"
 	"github.com/cloudoperators/greenhouse/pkg/test"
+
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+const (
+	mockDb   = "mock"
+	mockUsr  = "mock"
+	mockPwd  = "mock_pwd"
+	mockPort = 5004
 )
 
 var (
 	groupsServer *httptest.Server
+	mockPgTc     *postgres.PostgresContainer
 )
 
 func TestOrganization(t *testing.T) {
@@ -26,10 +40,29 @@ func TestOrganization(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
+	var err error
+	ctx := context.Background()
 	By("mocking SCIM server")
 	groupsServer = scim.ReturnDefaultGroupResponseMockServer()
 
-	test.RegisterController("organizationController", (&organizationpkg.OrganizationReconciler{Namespace: "default"}).SetupWithManager)
+	mockPgTc, err = startPgTC(ctx)
+	Expect(err).NotTo(HaveOccurred())
+
+	host, err := mockPgTc.Host(ctx)
+	Expect(err).NotTo(HaveOccurred())
+
+	port, err := mockPgTc.MappedPort(ctx, "5432/tcp")
+	Expect(err).NotTo(HaveOccurred())
+
+	netDB := sql.NetworkDB{
+		Host:     host,
+		Port:     uint16(port.Int()),
+		User:     mockUsr,
+		Password: mockPwd,
+		Database: mockDb,
+	}
+
+	test.RegisterController("organizationController", (&organizationpkg.OrganizationReconciler{Namespace: "default", NetworkDB: netDB}).SetupWithManager)
 	test.RegisterWebhook("orgWebhook", admission.SetupOrganizationWebhookWithManager)
 	test.RegisterWebhook("teamWebhook", admission.SetupTeamWebhookWithManager)
 	test.RegisterWebhook("pluginDefinitionWebhook", admission.SetupPluginDefinitionWebhookWithManager)
@@ -41,6 +74,24 @@ var _ = BeforeSuite(func() {
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
 	groupsServer.Close()
+	err := testcontainers.TerminateContainer(mockPgTc)
+	Expect(err).NotTo(HaveOccurred())
 
 	test.TestAfterSuite()
 })
+
+func startPgTC(ctx context.Context) (*postgres.PostgresContainer, error) {
+	return postgres.Run(ctx, "postgres:16-alpine",
+		postgres.WithDatabase(mockDb),
+		postgres.WithUsername(mockUsr),
+		postgres.WithPassword(mockPwd),
+		testcontainers.WithWaitStrategy(
+			// First, we wait for the container to log readiness twice.
+			// This is because it will restart itself after the first startup.
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2),
+			// Then, we wait for docker to actually serve the port on localhost.
+			// For non-linux OSes like Mac and Windows, Docker or Rancher Desktop will have to start a separate proxy.
+			// Without this, the tests will be flaky on those OSes!
+			wait.ForListeningPort("5432/tcp"),
+		))
+}
