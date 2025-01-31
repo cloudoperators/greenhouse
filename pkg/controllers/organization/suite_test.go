a3f1211b7edd4c73ff355fb538106393b15c352e
abhijith-darshan <abhijith.ravindra@sap.com>
2025-02-01 00:29:44 +0100

    (chore): mock dex storage interface in unit tests

--- pkg/controllers/organization/suite_test.go
+++ pkg/controllers/organization/suite_test.go
@@ -9,9 +9,12 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/cloudoperators/greenhouse/pkg/admission"
 	organizationpkg "github.com/cloudoperators/greenhouse/pkg/controllers/organization"
+	dexstore "github.com/cloudoperators/greenhouse/pkg/dex/store"
+	"github.com/cloudoperators/greenhouse/pkg/mocks"
 	"github.com/cloudoperators/greenhouse/pkg/scim"
 	"github.com/cloudoperators/greenhouse/pkg/test"
 )
@@ -28,8 +31,8 @@ func TestOrganization(t *testing.T) {
 var _ = BeforeSuite(func() {
 	By("mocking SCIM server")
 	groupsServer = scim.ReturnDefaultGroupResponseMockServer()
-
-	test.RegisterController("organizationController", (&organizationpkg.OrganizationReconciler{Namespace: "default"}).SetupWithManager)
+	dexter := dexMocks()
+	test.RegisterController("organizationController", (&organizationpkg.OrganizationReconciler{Namespace: "default", Dexter: dexter}).SetupWithManager)
 	test.RegisterWebhook("orgWebhook", admission.SetupOrganizationWebhookWithManager)
 	test.RegisterWebhook("teamWebhook", admission.SetupTeamWebhookWithManager)
 	test.RegisterWebhook("pluginDefinitionWebhook", admission.SetupPluginDefinitionWebhookWithManager)
@@ -44,3 +47,11 @@ var _ = AfterSuite(func() {
 
 	test.TestAfterSuite()
 })
+
+func dexMocks() dexstore.Dexter {
+	dexter := &mocks.MockDexter{}
+	dexter.On("CreateUpdateConnector", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	dexter.On("CreateUpdateOauth2Client", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	dexter.On("GetBackend").Return(dexstore.K8s)
+	return dexter
+}
