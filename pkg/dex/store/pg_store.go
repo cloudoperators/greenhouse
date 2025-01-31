cf7b2d8a5a89c0497426e6fa023bf6a54b7782fe
abhijith-darshan <abhijith.ravindra@sap.com>
2025-02-01 00:24:32 +0100

    (chore): adds dex postgres storage adapter
    additionally while creating OAuth2Client in postgres it also generates client secret in the organization namespace

---
+++ pkg/dex/store/pg_store.go
@@ -0,0 +1,156 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package store
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/dexidp/dex/storage"
+	"github.com/dexidp/dex/storage/sql"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	greenhouseapisv1alpha1 "github.com/cloudoperators/greenhouse/pkg/apis/greenhouse/v1alpha1"
+	"github.com/cloudoperators/greenhouse/pkg/clientutil"
+	"github.com/cloudoperators/greenhouse/pkg/common"
+	"github.com/cloudoperators/greenhouse/pkg/util"
+)
+
+type pgDex struct {
+	storage storage.Storage
+	backend string
+}
+
+const (
+	hostEnv   = "DB_HOST"
+	portEnv   = "DB_PORT"
+	userEnv   = "DB_USER"
+	passEnv   = "DB_PASSWORD"
+	dbNameEnv = "DB_DATABASE"
+)
+
+func newPostgresStore(logger *slog.Logger) (storage.Storage, error) {
+	var host, user, pass, database string
+	var port int
+	var err error
+	database = clientutil.GetEnvOrDefault(dbNameEnv, "postgres")
+	user = clientutil.GetEnvOrDefault(userEnv, "postgres")
+	port = clientutil.GetIntEnvWithDefault(portEnv, 5432)
+	if host, err = clientutil.GetEnv(hostEnv); err != nil {
+		return nil, err
+	}
+	if pass, err = clientutil.GetEnv(passEnv); err != nil {
+		return nil, err
+	}
+	cfg := &sql.Postgres{
+		SSL: sql.SSL{Mode: "disable"},
+		NetworkDB: sql.NetworkDB{
+			Host:     host,
+			Port:     uint16(port),
+			User:     user,
+			Password: pass,
+			Database: database,
+		},
+	}
+	return cfg.Open(logger)
+}
+
+func (p *pgDex) GetBackend() string {
+	return p.backend
+}
+
+func (p *pgDex) CreateUpdateConnector(ctx context.Context, _ client.Client, org *greenhouseapisv1alpha1.Organization, configByte []byte, _ string) error {
+	oidcConnector, err := p.storage.GetConnector(org.Name)
+	if err != nil && errors.Is(err, storage.ErrNotFound) {
+		if err = p.storage.CreateConnector(ctx, storage.Connector{
+			ID:     org.Name,
+			Type:   dexConnectorTypeGreenhouse,
+			Name:   cases.Title(language.English).String(org.Name),
+			Config: configByte,
+		}); err != nil {
+			return err
+		}
+		log.FromContext(ctx).Info("created dex connector in SQL storage", "name", org.Name)
+	}
+	if err != nil {
+		log.FromContext(ctx).Error(err, "failed to get dex connector in SQL storage", "name", org.Name)
+	}
+	err = p.storage.UpdateConnector(oidcConnector.ID, func(c storage.Connector) (storage.Connector, error) {
+		c.ID = org.Name
+		c.Type = dexConnectorTypeGreenhouse
+		c.Name = cases.Title(language.English).String(org.Name)
+		c.Config = configByte
+		return c, nil
+	})
+
+	if err != nil {
+		return err
+	}
+	log.FromContext(ctx).Info("updated dex connector in SQL storage", "name", org.Name)
+	return nil
+}
+
+func (p *pgDex) CreateUpdateOauth2Client(ctx context.Context, k8sClient client.Client, org *greenhouseapisv1alpha1.Organization, namespace string) error {
+	generatedClientSecret := getDeterministicSecret(org.Name, org.GetResourceVersion(), org.GetUID())
+	oAuthClient, err := p.storage.GetClient(org.Name)
+	if err != nil && errors.Is(err, storage.ErrNotFound) {
+		if err = p.storage.CreateClient(ctx, storage.Client{
+			Public: true,
+			ID:     org.Name,
+			Secret: generatedClientSecret,
+			Name:   org.Name,
+			RedirectURIs: []string{
+				"http://localhost:8085",
+				"http://localhost:8000",
+				"https://dashboard." + common.DNSDomain,
+				fmt.Sprintf("https://%s.dashboard.%s", org.Name, common.DNSDomain),
+			},
+		}); err != nil {
+			return err
+		}
+		log.FromContext(ctx).Info("created oauth2client", "name", org.Name)
+		return nil
+	}
+	if err != nil {
+		log.FromContext(ctx).Error(err, "failed to get oauth2client", "name", org.Name)
+	}
+	err = p.storage.UpdateClient(oAuthClient.Name, func(authClient storage.Client) (storage.Client, error) {
+		authClient.Public = true
+		authClient.ID = org.Name
+		authClient.Secret = generatedClientSecret
+		authClient.Name = org.Name
+		for _, requiredRedirectURL := range []string{
+			"http://localhost:8085",
+			"http://localhost:8000",
+			"https://dashboard." + common.DNSDomain,
+			fmt.Sprintf("https://%s.dashboard.%s", org.Name, common.DNSDomain),
+		} {
+			authClient.RedirectURIs = util.AppendStringToSliceIfNotContains(requiredRedirectURL, authClient.RedirectURIs)
+		}
+		return authClient, nil
+	})
+	if err != nil {
+		return err
+	}
+	secret := prepareClientSecret(namespace, org.Name, generatedClientSecret)
+	err = writeCredentialsToNamespace(ctx, k8sClient, secret)
+	if err != nil {
+		return err
+	}
+	log.FromContext(ctx).Info("updated oauth2client", "name", org.Name)
+	return nil
+}
+
+func (p *pgDex) GetStorage() storage.Storage {
+	return p.storage
+}
+
+func (p *pgDex) Close() error {
+	return p.storage.Close()
+}
