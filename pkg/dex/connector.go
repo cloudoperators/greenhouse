69449bb998b1144f2a4d1663fd1f15d588b8fba1
abhijith-darshan <abhijith.ravindra@sap.com>
2025-01-31 15:32:57 +0100

    (chore): move to dex pkg

--- pkg/dex/connector.go
+++ pkg/dex/connector.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
 // SPDX-License-Identifier: Apache-2.0
 
-package idproxy
+package dex
 
 import (
 	"context"
