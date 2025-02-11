efc24d4a60fe050e4fc5717c1624242174b78d01
k.zagorski <k.zagorski@accenture.com>
2025-02-11 20:41:31 +0100

    feat(plugins): add e2e test for first helm release rollback, get helm release timeout from env var or default

---
+++ pkg/helm/utils.go
@@ -0,0 +1,17 @@
+package helm
+
+import (
+	"time"
+
+	"github.com/cloudoperators/greenhouse/pkg/clientutil"
+)
+
+// Default Greenhouse helm timeout in seconds for install, upgrade and rollback actions.
+const helmReleaseTimeoutSeconds int = 300
+
+// GetHelmTimeout gets a timeout for helm release install, upgrade and rollback actions.
+// Tries to get the value from HELM_RELEASE_TIMEOUT evironment variable, otherwise gets the default value.
+func GetHelmTimeout() time.Duration {
+	val := clientutil.GetIntEnvWithDefault("HELM_RELEASE_TIMEOUT", helmReleaseTimeoutSeconds)
+	return time.Duration(val) * time.Second
+}
