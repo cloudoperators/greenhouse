04ebe7c0eb4b6c0701c79ed40b7f4f02576db369
grzegorz-ciezkowski <grzegorz.ciezkowski@accenture.com>
2025-02-20 15:58:22 +0100

    feat(teamrbac) Create namespace flag (#920)

--- pkg/test/resources.go
+++ pkg/test/resources.go
@@ -305,6 +305,12 @@ func WithNamespaces(namespaces ...string) func(*greenhousev1alpha1.TeamRoleBindi
 	}
 }
 
+func WithCreateNamespace(createNamespaces bool) func(*greenhousev1alpha1.TeamRoleBinding) {
+	return func(trb *greenhousev1alpha1.TeamRoleBinding) {
+		trb.Spec.CreateNamespaces = createNamespaces
+	}
+}
+
 // NewTeamRoleBinding returns a greenhousev1alpha1.TeamRoleBinding object. Opts can be used to set the desired state of the TeamRoleBinding.
 func NewTeamRoleBinding(ctx context.Context, name, namespace string, opts ...func(*greenhousev1alpha1.TeamRoleBinding)) *greenhousev1alpha1.TeamRoleBinding {
 	trb := &greenhousev1alpha1.TeamRoleBinding{
