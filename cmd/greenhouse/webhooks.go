51a439451864cddc4f920706660ebb9cd41fa91f
k.zagorski <k.zagorski@accenture.com>
2025-03-19 18:42:48 +0100

    feat: fix SchemeBuilder reference for dex, fix webhook imports

--- cmd/greenhouse/webhooks.go
+++ cmd/greenhouse/webhooks.go
@@ -6,19 +6,19 @@ package main
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	admissionv1alpha1 "github.com/cloudoperators/greenhouse/internal/webhook/v1alpha1"
-	admissionv1alpha2 "github.com/cloudoperators/greenhouse/internal/webhook/v1alpha2"
+	webhookv1alpha1 "github.com/cloudoperators/greenhouse/internal/webhook/v1alpha1"
+	webhookv1alpha2 "github.com/cloudoperators/greenhouse/internal/webhook/v1alpha2"
 )
 
 var knownWebhooks = map[string]func(mgr ctrl.Manager) error{
-	"cluster":                  admissionv1alpha1.SetupClusterWebhookWithManager,
-	"secrets":                  admissionv1alpha1.SetupSecretWebhookWithManager,
-	"organization":             admissionv1alpha1.SetupOrganizationWebhookWithManager,
-	"pluginDefinition":         admissionv1alpha1.SetupPluginDefinitionWebhookWithManager,
-	"plugin":                   admissionv1alpha1.SetupPluginWebhookWithManager,
-	"pluginPreset":             admissionv1alpha1.SetupPluginPresetWebhookWithManager,
-	"teamrole":                 admissionv1alpha1.SetupTeamRoleWebhookWithManager,
-	"teamrolebinding_v1alpha1": admissionv1alpha1.SetupTeamRoleBindingWebhookWithManager,
-	"teamrolebinding_v1alpha2": admissionv1alpha2.SetupTeamRoleBindingWebhookWithManager,
-	"team":                     admissionv1alpha1.SetupTeamWebhookWithManager,
+	"cluster":                  webhookv1alpha1.SetupClusterWebhookWithManager,
+	"secrets":                  webhookv1alpha1.SetupSecretWebhookWithManager,
+	"organization":             webhookv1alpha1.SetupOrganizationWebhookWithManager,
+	"pluginDefinition":         webhookv1alpha1.SetupPluginDefinitionWebhookWithManager,
+	"plugin":                   webhookv1alpha1.SetupPluginWebhookWithManager,
+	"pluginPreset":             webhookv1alpha1.SetupPluginPresetWebhookWithManager,
+	"teamrole":                 webhookv1alpha1.SetupTeamRoleWebhookWithManager,
+	"teamrolebinding_v1alpha1": webhookv1alpha1.SetupTeamRoleBindingWebhookWithManager,
+	"teamrolebinding_v1alpha2": webhookv1alpha2.SetupTeamRoleBindingWebhookWithManager,
+	"team":                     webhookv1alpha1.SetupTeamWebhookWithManager,
 }
