0ce1d35c6f1c46716d5f0af259c25ef483c5f44d
abhijith-darshan <abhijith.ravindra@sap.com>
2025-05-19 11:19:52 +0200

    (chore): addresses race condition
    organization unit tests attempt to create org only if it does not exist

--- internal/test/setup.go
+++ internal/test/setup.go
@@ -116,8 +116,14 @@ func (t *TestSetup) CreateOrganizationWithOIDCConfig(ctx context.Context, orgNam
 // CreateOrganization creates a Organization within the TestSetup and returns the created Organization resource.
 func (t *TestSetup) CreateOrganization(ctx context.Context, name string, opts ...func(*greenhousev1alpha1.Organization)) *greenhousev1alpha1.Organization {
 	GinkgoHelper()
-	org := NewOrganization(ctx, name, opts...)
-	Expect(t.Create(ctx, org)).Should(Succeed(), "there should be no error creating the Organization")
+	org := &greenhousev1alpha1.Organization{}
+	err := t.Get(ctx, client.ObjectKey{Name: name}, org)
+	if client.IgnoreNotFound(err) == nil {
+		org := NewOrganization(ctx, name, opts...)
+		Expect(t.Create(ctx, org)).Should(Succeed(), "there should be no error creating the Organization")
+		return org
+	}
+	Expect(err).NotTo(HaveOccurred(), "there should be no error getting the Organization")
 	return org
 }
 
