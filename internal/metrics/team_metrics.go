1bab138d3de44455ab185ecb5847254950ace3b7
k.zagorski <k.zagorski@accenture.com>
2025-07-18 15:48:44 +0200

    refactor: move team metrics into the internal metrics package
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

---
+++ internal/metrics/team_metrics.go
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/api/v1alpha1"
+)
+
+var (
+	membersCountMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "greenhouse_team_members_count",
+			Help: "Members count in team",
+		},
+		[]string{"namespace", "team"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(membersCountMetric)
+}
+
+func UpdateTeamMembersCountMetric(team *greenhousev1alpha1.Team, membersCount int) {
+	membersCountMetric.With(prometheus.Labels{
+		"namespace": team.Namespace,
+		"team":      team.Name,
+	}).Set(float64(membersCount))
+}
