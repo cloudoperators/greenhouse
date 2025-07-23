9bc8fc71f8e6400dd307aa6bba88216796eae95a
k.zagorski <k.zagorski@accenture.com>
2025-07-23 16:15:05 +0200

    feat: add another test to common metrics
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

--- internal/metrics/common_metrics_test.go
+++ internal/metrics/common_metrics_test.go
@@ -7,37 +7,30 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	prometheusTest "github.com/prometheus/client_golang/prometheus/testutil"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 
 	"github.com/cloudoperators/greenhouse/internal/metrics"
 
-	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/api/v1alpha1"
 	"github.com/cloudoperators/greenhouse/internal/test"
 )
 
 var _ = Describe("Common Metrics", Ordered, func() {
 	var (
 		remoteEnvTest *envtest.Environment
+		setup         *test.TestSetup
 	)
 
 	BeforeAll(func() {
 		_, _, remoteEnvTest, _ = test.StartControlPlane("6888", false, false)
+		setup = test.NewTestSetup(test.Ctx, test.K8sClient, "commonmetrics")
 	})
 
 	AfterAll(func() {
 		Expect(remoteEnvTest.Stop()).To(Succeed(), "there should be no error stopping the remote environment")
 	})
 
-	It("Should update the OwnedByLabelMissingMetric when reconciler is calling", func() {
-		const clusterName = "test-cluster-a"
-		setup := test.NewTestSetup(test.Ctx, test.K8sClient, "commonmetrics")
-		cluster := &greenhousev1alpha1.Cluster{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      clusterName,
-				Namespace: setup.Namespace(),
-			},
-		}
+	It("Should update the OwnedByLabelMissingMetric when Cluster reconciler is calling", func() {
+		cluster := test.NewCluster(test.Ctx, "test-cluster-a", setup.Namespace())
 
 		metrics.UpdateOwnedByLabelMissingMetric(cluster, true)
 
@@ -49,6 +42,22 @@ var _ = Describe("Common Metrics", Ordered, func() {
 
 		counterAfter = prometheusTest.ToFloat64(metrics.OwnedByLabelMissingGauge.
 			WithLabelValues(cluster.Kind, cluster.Name, cluster.Namespace))
+		Expect(counterAfter).To(BeEquivalentTo(0))
+	})
+
+	It("Should update the OwnedByLabelMissingMetric when Plugin reconciler is calling", func() {
+		plugin := test.NewPlugin(test.Ctx, "test-plugin-a", setup.Namespace())
+
+		metrics.UpdateOwnedByLabelMissingMetric(plugin, true)
+
+		counterAfter := prometheusTest.ToFloat64(metrics.OwnedByLabelMissingGauge.
+			WithLabelValues(plugin.Kind, plugin.Name, plugin.Namespace))
+		Expect(counterAfter).To(BeEquivalentTo(1))
+
+		metrics.UpdateOwnedByLabelMissingMetric(plugin, false)
+
+		counterAfter = prometheusTest.ToFloat64(metrics.OwnedByLabelMissingGauge.
+			WithLabelValues(plugin.Kind, plugin.Name, plugin.Namespace))
 		Expect(counterAfter).To(BeEquivalentTo(0))
 	})
 })
