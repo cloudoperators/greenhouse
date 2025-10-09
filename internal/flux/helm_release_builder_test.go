7c52a73a27c198f45249ac848cea467f5afb45e5
Ivo Gosemann <ivo.gosemann@sap.com>
2025-10-09 15:34:28 +0200

    fix(plugins): ensure storage and target namespace are the same
    StorageNamespace was defaulted to Namespace of the HelmRelease resource. This leads to release secret and helm release being in different namespaces.
    
    With this change helm cli can be used again in the same namespace as the helm release

--- internal/flux/helm_release_builder_test.go
+++ internal/flux/helm_release_builder_test.go
@@ -23,6 +23,7 @@ func TestHelmReleaseBuilder_CreatesValidHelmRelease(t *testing.T) {
 		WithInterval(10 * time.Minute).
 		WithTimeout(2 * time.Minute).
 		WithReleaseName("custom-release").
+		WithStorageNamespace("target-ns").
 		WithTargetNamespace("target-ns")
 
 	spec, err := builder.Build()
@@ -31,6 +32,7 @@ func TestHelmReleaseBuilder_CreatesValidHelmRelease(t *testing.T) {
 	assert.Equal(t, "1.2.3", spec.Chart.Spec.Version)
 	assert.Equal(t, 5, *spec.MaxHistory)
 	assert.Equal(t, "custom-release", spec.ReleaseName)
+	assert.Equal(t, "target-ns", spec.StorageNamespace)
 	assert.Equal(t, "target-ns", spec.TargetNamespace)
 }
 
