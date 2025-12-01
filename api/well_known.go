3823c30dcbfee21d8994cf189fc8cc0c29b6306a
abhijith-darshan <abhijith.ravindra@sap.com>
2025-12-01 12:59:02 +0100

    (chore): implements plugin-plugin integration
    extract values from another plugin via CEL expressions
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): adds tracked object to status
    
    refactor values resolver
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): feature toggle for p2p integration
    
    error out if feature toggle not enabled
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): remove untrackedObjects from annotation
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): avoid self-tracking circular dependency
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): adds basic unit testing
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): generate openapi spec
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): fix lint and tests
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): move plugin integration test to own suite
    
    fix lint and tests for preset and flux
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): revert to inline values for helm release
    
    write additional test cases
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): add integration feature flag to helm values
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): fix bitnamicharts/nginx version in tests
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): remove validation pattern for CEL
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>
    
    (chore): reduce function spillover
    
    Signed-off-by: abhijith-darshan <abhijith.ravindra@sap.com>

--- api/well_known.go
+++ api/well_known.go
@@ -69,6 +69,15 @@ const (
 
 	// LabelKeyCatalogSource is used to identify the source of the owning catalog resource of (Cluster)PluginDefinitions.
 	LabelKeyCatalogSource = "greenhouse.sap/catalog-source"
+
+	// LabelKeyPluginIntegration is used to identify resources that are part of plugin-plugin integration scenarios.
+	LabelKeyPluginIntegration = "greenhouse.sap/integration"
+	// LabelValuePluginIntegration will be used as list selector value to identify resources that are part of plugin-plugin integration scenarios.
+	LabelValuePluginIntegration = "true"
+	// AnnotationKeyPluginTackingID is used to identify the resources that are resolving values from the tracked plugin.
+	AnnotationKeyPluginTackingID = "greenhouse.sap/tracking-id"
+
+	FluxReconcileRequestAnnotation = "reconcile.fluxcd.io/requestedAt"
 )
 
 // TeamRole and TeamRoleBinding constants
@@ -126,7 +135,6 @@ const (
 	ClusterConnectivityOIDC           = "oidc"
 	GreenhouseHelmDeliveryToolLabel   = "greenhouse.sap/deployment-tool"
 	GreenhouseHelmDeliveryToolFlux    = "flux"
-	FluxReconcileRequestAnnotation    = "reconcile.fluxcd.io/requestedAt"
 )
 
 const (
