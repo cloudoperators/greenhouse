639ef07e1cfdfd4a51fef1071a76dfec067c2fbd
Zaggy21 <k.zaggy@gmail.com>
2026-01-12 15:38:35 +0100

    remove conversion e2e tests
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

--- api/v1alpha2/teamrolebinding_conversion.go
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Greenhouse contributors
-// SPDX-License-Identifier: Apache-2.0
-
-package v1alpha2
-
-// Hub marks this type as a conversion hub. See: https://book.kubebuilder.io/multiversion-tutorial/conversion
-func (*TeamRoleBinding) Hub() {}
