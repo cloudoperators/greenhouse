7775255b40539eca0de2d40d1e3bc7940ae2a428
Zaggy21 <k.zaggy@gmail.com>
2026-01-14 12:08:12 +0100

    fix after merge, extract test suite into its own file
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

--- pkg/lifecycle/suite_test.go
+++ pkg/lifecycle/suite_test.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Greenhouse contributors
 // SPDX-License-Identifier: Apache-2.0
 
-package lifecycle
+package lifecycle_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
