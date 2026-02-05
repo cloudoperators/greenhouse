49e02c52a07ed7d8ded04963369d8265abd96197
Krzysztof Zagorski <k.zaggy@gmail.com>
2026-02-05 15:58:36 +0100

    fix(pluginpresets): recreate Plugin when deleted (#1768)
    * fix: predicates for Plugins in PluginPresetController
    
    On-behalf-of: @SAP krzysztof.zagorski@sap.com
    
    * move IgnoreDeletingResources to its own predicate func, fix linter suggestion
    
    On-behalf-of: @SAP krzysztof.zagorski@sap.com
    
    * remove unnecessary DeleteFunc line from predicate, add UID check to the test
    
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

--- internal/clientutil/predicates.go
+++ internal/clientutil/predicates.go
@@ -78,7 +78,6 @@ func PredicatePluginWithStatusReadyChange() predicate.Predicate {
 			}
 			return oldReadyCondition.Status != newReadyCondition.Status
 		},
-		DeleteFunc:  func(_ event.DeleteEvent) bool { return false },
 		GenericFunc: func(_ event.GenericEvent) bool { return false },
 	}
 }
