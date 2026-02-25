b3a4c1b73c9be2da2d2bddd6c662d5a581531b9d
Zaggy21 <k.zaggy@gmail.com>
2026-02-25 15:40:47 +0100

    restructure the authz e2e tests, remove redundant ones
    On-behalf-of: @SAP krzysztof.zagorski@sap.com

--- e2e/authz/e2e_test.go
+++ e2e/authz/e2e_test.go
@@ -36,11 +36,11 @@ var (
 	testStartTime time.Time
 
 	// Test resources
-	teamObservability *greenhousev1alpha1.Team
-	teamDemo          *greenhousev1alpha1.Team
-	pluginDefPodInfo  *greenhousev1alpha1.PluginDefinition
-	pluginPodInfoObs  *greenhousev1alpha1.Plugin
-	pluginPodInfoDemo *greenhousev1alpha1.Plugin
+	teamDemo         *greenhousev1alpha1.Team
+	teamForbidden    *greenhousev1alpha1.Team
+	pluginDefPodInfo *greenhousev1alpha1.PluginDefinition
+	pluginDemo       *greenhousev1alpha1.Plugin
+	pluginForbidden  *greenhousev1alpha1.Plugin
 )
 
 func TestE2e(t *testing.T) {
@@ -63,12 +63,12 @@ var _ = BeforeSuite(func() {
 	testStartTime = time.Now().UTC()
 
 	By("creating test Teams")
-	teamObservability = test.NewTeam(ctx, "observability", env.TestNamespace, test.WithTeamLabel(greenhouseapis.LabelKeySupportGroup, "true"))
-	err = adminClient.Create(ctx, teamObservability)
-	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create Team observability")
 	teamDemo = test.NewTeam(ctx, "demo", env.TestNamespace, test.WithTeamLabel(greenhouseapis.LabelKeySupportGroup, "true"))
 	err = adminClient.Create(ctx, teamDemo)
 	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create Team demo")
+	teamForbidden = test.NewTeam(ctx, "forbidden", env.TestNamespace, test.WithTeamLabel(greenhouseapis.LabelKeySupportGroup, "true"))
+	err = adminClient.Create(ctx, teamForbidden)
+	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create Team forbidden")
 
 	By("onboarding remote cluster")
 	shared.OnboardRemoteCluster(ctx, adminClient, env.RemoteKubeConfigBytes, remoteClusterName, env.TestNamespace, teamDemo.Name)
@@ -78,229 +78,131 @@ var _ = BeforeSuite(func() {
 	err = adminClient.Create(ctx, pluginDefPodInfo)
 	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create plugindefinition-podinfo")
 
-	By("creating plugin-podinfo-obs")
-	pluginPodInfoObs = test.NewPlugin(ctx, "podinfo-obs", env.TestNamespace,
-		test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamObservability.Name),
-		test.WithPluginDefinition(pluginDefPodInfo.Name),
-		test.WithCluster(remoteClusterName),
-	)
-	err = adminClient.Create(ctx, pluginPodInfoObs)
-	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create plugin-podinfo-obs")
-	test.EventuallyCreated(ctx, adminClient, pluginPodInfoObs)
-
-	By("creating plugin-podinfo-demo")
-	pluginPodInfoDemo = test.NewPlugin(ctx, "podinfo-demo", env.TestNamespace,
+	By("creating plugin-demo")
+	pluginDemo = test.NewPlugin(ctx, "plugin-demo", env.TestNamespace,
 		test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamDemo.Name),
 		test.WithPluginDefinition(pluginDefPodInfo.Name),
 		test.WithCluster(remoteClusterName),
 	)
-	err = adminClient.Create(ctx, pluginPodInfoDemo)
-	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create plugin-podinfo-demo")
-	test.EventuallyCreated(ctx, adminClient, pluginPodInfoDemo)
+	err = adminClient.Create(ctx, pluginDemo)
+	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create plugin-demo")
+	test.EventuallyCreated(ctx, adminClient, pluginDemo)
+
+	By("creating plugin-forbidden")
+	pluginForbidden = test.NewPlugin(ctx, "plugin-forbidden", env.TestNamespace,
+		test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamForbidden.Name),
+		test.WithPluginDefinition(pluginDefPodInfo.Name),
+		test.WithCluster(remoteClusterName),
+	)
+	err = adminClient.Create(ctx, pluginForbidden)
+	Expect(client.IgnoreAlreadyExists(err)).ToNot(HaveOccurred(), "should create plugin-forbidden")
+	test.EventuallyCreated(ctx, adminClient, pluginForbidden)
 })
 
 var _ = AfterSuite(func() {
 	By("cleaning up test resources")
-	test.EventuallyDeleted(ctx, adminClient, pluginPodInfoDemo)
-	test.EventuallyDeleted(ctx, adminClient, pluginPodInfoObs)
+	test.EventuallyDeleted(ctx, adminClient, pluginDemo)
+	test.EventuallyDeleted(ctx, adminClient, pluginForbidden)
 	test.EventuallyDeleted(ctx, adminClient, pluginDefPodInfo)
 
 	shared.OffBoardRemoteCluster(ctx, adminClient, remoteClient, testStartTime, remoteClusterName, env.TestNamespace)
 
-	test.EventuallyDeleted(ctx, adminClient, teamObservability)
 	test.EventuallyDeleted(ctx, adminClient, teamDemo)
+	test.EventuallyDeleted(ctx, adminClient, teamForbidden)
 
 	env.GenerateGreenhouseControllerLogs(ctx, testStartTime)
 })
 
 var _ = Describe("Authorization Webhook E2E", Ordered, func() {
 	Describe("GET operations", func() {
-		Context("when user has matching support-group", func() {
-			It("should allow user1 to GET podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should allow demo-user to GET their team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoObs), plugin)
-				Expect(err).ToNot(HaveOccurred(), "user1 with support-group:observability should be able to GET podinfo-obs")
-			})
-
-			It("should allow user2 to GET podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoDemo), plugin)
-				Expect(err).ToNot(HaveOccurred(), "user2 with support-group:demo should be able to GET podinfo-demo")
-			})
+			plugin := &greenhousev1alpha1.Plugin{}
+			err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginDemo), plugin)
+			Expect(err).ToNot(HaveOccurred(), "demo-user should be able to GET plugin-demo")
 		})
 
-		Context("when user does not have matching support-group", func() {
-			It("should deny user1 to GET podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should deny demo-user to GET another team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoDemo), plugin)
-				Expect(err).To(HaveOccurred(), "user1 with support-group:observability should NOT be able to GET podinfo-demo")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
-
-			It("should deny user2 to GET podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoObs), plugin)
-				Expect(err).To(HaveOccurred(), "user2 with support-group:demo should NOT be able to GET podinfo-obs")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
+			plugin := &greenhousev1alpha1.Plugin{}
+			err = impClient.Get(ctx, client.ObjectKeyFromObject(pluginForbidden), plugin)
+			Expect(err).To(HaveOccurred(), "demo-user should NOT be able to GET plugin-forbidden")
+			Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
 		})
 	})
 
 	Describe("UPDATE operations", func() {
-		Context("when user has matching support-group", func() {
-			It("should allow user1 to UPDATE podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should allow demo-user to UPDATE their team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoObs), plugin)
-				Expect(err).ToNot(HaveOccurred(), "should get current plugin")
+			plugin := &greenhousev1alpha1.Plugin{}
+			err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginDemo), plugin)
+			Expect(err).ToNot(HaveOccurred(), "should get current plugin")
 
-				plugin.Spec.DisplayName = "podinfo observability updated"
-				err = impClient.Update(ctx, plugin)
-				Expect(err).ToNot(HaveOccurred(), "user1 with support-group:observability should be able to UPDATE podinfo-obs")
-			})
-
-			It("should allow user2 to UPDATE podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoDemo), plugin)
-				Expect(err).ToNot(HaveOccurred(), "should get current plugin")
-
-				plugin.Spec.DisplayName = "podinfo demo updated"
-				err = impClient.Update(ctx, plugin)
-				Expect(err).ToNot(HaveOccurred(), "user2 with support-group:demo should be able to UPDATE podinfo-demo")
-			})
+			plugin.Spec.DisplayName = "plugin-demo updated"
+			err = impClient.Update(ctx, plugin)
+			Expect(err).ToNot(HaveOccurred(), "demo-user should be able to UPDATE plugin-demo")
 		})
 
-		Context("when user does not have matching support-group", func() {
-			It("should deny user1 to UPDATE podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should deny demo-user to UPDATE another team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoDemo), plugin)
-				Expect(err).ToNot(HaveOccurred(), "should get current plugin")
+			plugin := &greenhousev1alpha1.Plugin{}
+			err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginForbidden), plugin)
+			Expect(err).ToNot(HaveOccurred(), "should get current plugin")
 
-				plugin.Spec.DisplayName = "podinfo demo updated"
-				err = impClient.Update(ctx, plugin)
-				Expect(err).To(HaveOccurred(), "user1 with support-group:observability should NOT be able to UPDATE podinfo-demo")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
-
-			It("should deny user2 to UPDATE podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				plugin := &greenhousev1alpha1.Plugin{}
-				err = adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoObs), plugin)
-				Expect(err).ToNot(HaveOccurred(), "should get current plugin")
-
-				plugin.Spec.DisplayName = "podinfo observability updated"
-				err = impClient.Update(ctx, plugin)
-				Expect(err).To(HaveOccurred(), "user2 with support-group:demo should NOT be able to UPDATE podinfo-obs")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
+			plugin.Spec.DisplayName = "plugin-forbidden updated"
+			err = impClient.Update(ctx, plugin)
+			Expect(err).To(HaveOccurred(), "demo-user should NOT be able to UPDATE plugin-forbidden")
+			Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
 		})
 	})
 
 	Describe("DELETE operations", func() {
-		Context("when user has matching support-group", func() {
-			It("should allow user1 to DELETE and recreate podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should allow demo-user to DELETE and recreate their team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				By("deleting the plugin")
-				err = impClient.Delete(ctx, pluginPodInfoObs)
-				Expect(err).ToNot(HaveOccurred(), "user1 with support-group:observability should be able to DELETE podinfo-obs")
+			By("deleting the plugin")
+			err = impClient.Delete(ctx, pluginDemo)
+			Expect(err).ToNot(HaveOccurred(), "demo-user should be able to DELETE plugin-demo")
 
-				By("waiting for plugin to be deleted")
-				Eventually(func(g Gomega) {
-					plugin := &greenhousev1alpha1.Plugin{}
-					err := adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoObs), plugin)
-					g.Expect(apierrors.IsNotFound(err)).To(BeTrue(), "plugin should be deleted")
-				}, 30*time.Second, 2*time.Second).Should(Succeed())
+			By("waiting for plugin to be deleted")
+			Eventually(func() bool {
+				plugin := &greenhousev1alpha1.Plugin{}
+				err := adminClient.Get(ctx, client.ObjectKeyFromObject(pluginDemo), plugin)
+				return apierrors.IsNotFound(err)
+			}).Should(BeTrue(), "plugin should be deleted")
 
-				By("recreating the plugin")
-				pluginPodInfoObs = test.NewPlugin(ctx, "podinfo-obs", env.TestNamespace,
-					test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamObservability.Name),
-					test.WithPluginDefinition(pluginDefPodInfo.Name),
-					test.WithCluster(remoteClusterName),
-				)
-				err = adminClient.Create(ctx, pluginPodInfoObs)
-				Expect(err).ToNot(HaveOccurred(), "should recreate plugin-podinfo-obs")
+			By("recreating the plugin")
+			pluginDemo = test.NewPlugin(ctx, "plugin-demo", env.TestNamespace,
+				test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamDemo.Name),
+				test.WithPluginDefinition(pluginDefPodInfo.Name),
+				test.WithCluster(remoteClusterName),
+			)
+			err = adminClient.Create(ctx, pluginDemo)
+			Expect(err).ToNot(HaveOccurred(), "should recreate plugin-demo")
 
-				By("waiting for plugin to be recreated")
-				test.EventuallyCreated(ctx, adminClient, pluginPodInfoObs)
-			})
-
-			It("should allow user2 to DELETE and recreate podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				By("deleting the plugin")
-				err = impClient.Delete(ctx, pluginPodInfoDemo)
-				Expect(err).ToNot(HaveOccurred(), "user2 with support-group:demo should be able to DELETE podinfo-demo")
-
-				By("waiting for plugin to be deleted")
-				Eventually(func(g Gomega) {
-					plugin := &greenhousev1alpha1.Plugin{}
-					err := adminClient.Get(ctx, client.ObjectKeyFromObject(pluginPodInfoDemo), plugin)
-					g.Expect(apierrors.IsNotFound(err)).To(BeTrue(), "plugin should be deleted")
-				}, 30*time.Second, 2*time.Second).Should(Succeed())
-
-				By("recreating the plugin")
-				pluginPodInfoDemo = test.NewPlugin(ctx, "podinfo-demo", env.TestNamespace,
-					test.WithPluginLabel(greenhouseapis.LabelKeyOwnedBy, teamDemo.Name),
-					test.WithPluginDefinition(pluginDefPodInfo.Name),
-					test.WithCluster(remoteClusterName),
-				)
-				err = adminClient.Create(ctx, pluginPodInfoDemo)
-				Expect(err).ToNot(HaveOccurred(), "should recreate plugin-podinfo-demo")
-
-				By("waiting for plugin to be recreated")
-				test.EventuallyCreated(ctx, adminClient, pluginPodInfoDemo)
-			})
+			By("waiting for plugin to be recreated")
+			test.EventuallyCreated(ctx, adminClient, pluginDemo)
 		})
 
-		Context("when user does not have matching support-group", func() {
-			It("should deny user1 to DELETE podinfo-demo plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user1", []string{"support-group:observability"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
+		It("should deny demo-user to DELETE another team's plugin", func() {
+			impClient, err := createImpersonatedClient("demo-user", []string{"support-group:demo"})
+			Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
 
-				plugin := &greenhousev1alpha1.Plugin{}
-				plugin.Name = pluginPodInfoDemo.Name
-				plugin.Namespace = pluginPodInfoDemo.Namespace
-				err = impClient.Delete(ctx, plugin)
-				Expect(err).To(HaveOccurred(), "user1 with support-group:observability should NOT be able to DELETE podinfo-demo")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
-
-			It("should deny user2 to DELETE podinfo-obs plugin", func() {
-				impClient, err := createImpersonatedClient("my-test-user2", []string{"support-group:demo"})
-				Expect(err).ToNot(HaveOccurred(), "should create impersonated client")
-
-				plugin := &greenhousev1alpha1.Plugin{}
-				plugin.Name = pluginPodInfoObs.Name
-				plugin.Namespace = pluginPodInfoObs.Namespace
-				err = impClient.Delete(ctx, plugin)
-				Expect(err).To(HaveOccurred(), "user2 with support-group:demo should NOT be able to DELETE podinfo-obs")
-				Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
-			})
+			plugin := &greenhousev1alpha1.Plugin{}
+			plugin.Name = pluginForbidden.Name
+			plugin.Namespace = pluginForbidden.Namespace
+			err = impClient.Delete(ctx, plugin)
+			Expect(err).To(HaveOccurred(), "demo-user should NOT be able to DELETE plugin-forbidden")
+			Expect(apierrors.IsForbidden(err)).To(BeTrue(), "error should indicate forbidden access")
 		})
 	})
 })
