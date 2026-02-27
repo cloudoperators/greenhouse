fc17b50d17adc080a257f6b16d6b44ca6f23a4e6
Mikolaj Kucinski <osidu12@gmail.com>
2026-02-27 12:01:04 +0100

    fix: linter
    On-behalf-of: @SAP <mikolaj.kucinski@sap.com>

--- internal/imagemirror/replicator.go
+++ internal/imagemirror/replicator.go
@@ -16,7 +16,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
 )
 
 // ManifestFetcher matches the crane.Manifest signature.
