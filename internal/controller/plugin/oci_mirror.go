599ba4313c7ec179b6aead321f8e1d2a16a84b87
Mikolaj Kucinski <osidu12@gmail.com>
2026-04-01 11:07:23 +0200

    feat(plugin): add ensureImageReplication adapter for container image pre-replication
    On-behalf-of: @SAP <mikolaj.kucinski@sap.com>

--- internal/controller/plugin/oci_mirror.go
+++ internal/controller/plugin/oci_mirror.go
@@ -4,9 +4,14 @@
 package plugin
 
 import (
+	"context"
+	"fmt"
+
 	helmv2 "github.com/fluxcd/helm-controller/api/v2"
 	"github.com/fluxcd/pkg/apis/kustomize"
 
+	greenhousemetav1alpha1 "github.com/cloudoperators/greenhouse/api/meta/v1alpha1"
+	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/api/v1alpha1"
 	"github.com/cloudoperators/greenhouse/internal/ocimirror"
 )
 
@@ -33,4 +38,33 @@ func createRegistryMirrorPostRenderer(mirror *ocimirror.ImageMirror, renderedMan
 			Images: images,
 		},
 	}
+}
+
+// ensureImageReplication triggers pre-replication of container images found in renderedManifests.
+// It updates the Plugin's ImageReplication status and sets the ImageReplicationReady condition.
+func ensureImageReplication(ctx context.Context, mirror *ocimirror.ImageMirror, plugin *greenhousev1alpha1.Plugin, renderedManifests string) error {
+	replicated, err := mirror.ReplicateOCIArtifacts(ctx, renderedManifests, plugin.Status.ImageReplication)
+	plugin.Status.ImageReplication = replicated
+
+	if err != nil {
+		plugin.SetCondition(greenhousemetav1alpha1.FalseCondition(
+			greenhousev1alpha1.ImageReplicationReadyCondition,
+			greenhousev1alpha1.ImageReplicationFailedReason,
+			err.Error()))
+		return fmt.Errorf("image replication failed for Plugin %s: %w", plugin.Name, err)
+	}
+
+	if len(replicated) == 0 {
+		plugin.SetCondition(greenhousemetav1alpha1.TrueCondition(
+			greenhousev1alpha1.ImageReplicationReadyCondition,
+			greenhousev1alpha1.ImageReplicationNotConfiguredReason,
+			"no container images matched configured mirrors"))
+		return nil
+	}
+
+	plugin.SetCondition(greenhousemetav1alpha1.TrueCondition(
+		greenhousev1alpha1.ImageReplicationReadyCondition,
+		greenhousev1alpha1.ImageReplicationSucceededReason,
+		fmt.Sprintf("replicated %d container images", len(replicated))))
+	return nil
 }
