790312ad02b0f1e05781b53cb6aa5a37826d7b8a
Mikolaj Kucinski <osidu12@gmail.com>
2026-04-28 14:35:51 +0200

    feat(e2e): add OCI replication e2e test suite for chart and image replication
    On-behalf-of: @SAP <mikolaj.kucinski@sap.com>
    Signed-off-by: Mikolaj Kucinski <osidu12@gmail.com>

---
+++ e2e/plugindefinition/e2e_test.go
@@ -0,0 +1,65 @@
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build plugindefinitionE2E
+
+package plugindefinition
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/cloudoperators/greenhouse/e2e/plugindefinition/scenarios"
+	"github.com/cloudoperators/greenhouse/e2e/shared"
+	"github.com/cloudoperators/greenhouse/internal/clientutil"
+)
+
+var (
+	env           *shared.TestEnv
+	ctx           context.Context
+	adminClient   client.Client
+	testStartTime time.Time
+)
+
+func TestE2e(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PluginDefinition E2E Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx = context.Background()
+	env = shared.NewExecutionEnv()
+
+	var err error
+	adminClient, err = clientutil.NewK8sClientFromRestClientGetter(env.AdminRestClientGetter)
+	Expect(err).ToNot(HaveOccurred(), "there should be no error creating the admin client")
+
+	env = env.WithOrganization(ctx, adminClient, "./testdata/organization.yaml")
+
+	shared.SetupOCIMirroringForOrg(ctx, adminClient, env.TestNamespace)
+
+	testStartTime = time.Now().UTC()
+})
+
+var _ = AfterSuite(func() {
+	env.GenerateGreenhouseControllerLogs(ctx, testStartTime)
+	env.GenerateFluxControllerLogs(ctx, "helm-controller", testStartTime)
+})
+
+var _ = Describe("PluginDefinition E2E", Ordered, func() {
+	It("should replicate helm chart to registry mirror", func() {
+		scenarios.PDChartReplication(ctx, adminClient, env)
+	})
+
+	It("should fail chart replication for non-existent chart version", func() {
+		scenarios.PDChartReplicationFailure(ctx, adminClient, env)
+	})
+})
---
+++ e2e/shared/oci.go
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package shared
+
+import (
+	"context"
+
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	greenhousev1alpha1 "github.com/cloudoperators/greenhouse/api/v1alpha1"
+)
+
+// PrepareOCIPodInfoPluginDefinition returns a PluginDefinition pointing at the podinfo chart via the registry mirror URL.
+func PrepareOCIPodInfoPluginDefinition(name, namespace string) *greenhousev1alpha1.PluginDefinition {
+	return &greenhousev1alpha1.PluginDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: greenhousev1alpha1.PluginDefinitionSpec{
+			Description: "podinfo for OCI replication e2e",
+			Version:     "6.7.1",
+			HelmChart: &greenhousev1alpha1.HelmChartReference{
+				Name:       "podinfo",
+				Repository: "oci://registry:5000/greenhouse-ghcr-io-mirror/stefanprodan/charts",
+				Version:    "6.7.1",
+			},
+		},
+	}
+}
+
+// CreateMirrorConfigMap creates or updates the OCI registry mirror ConfigMap in the given namespace
+// with the standard mirror configuration used in e2e tests.
+func CreateMirrorConfigMap(ctx context.Context, adminClient client.Client, namespace string) {
+	mirrorCM := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "oci-replication-config",
+			Namespace: namespace,
+		},
+	}
+	_, err := controllerutil.CreateOrUpdate(ctx, adminClient, mirrorCM, func() error {
+		mirrorCM.Data = map[string]string{
+			"containerRegistryConfig": `primaryMirror: "registry:5000"
+registryMirrors:
+  ghcr.io:
+    baseDomain: "registry:5000"
+    subPath: "greenhouse-ghcr-io-mirror"`,
+		}
+		return nil
+	})
+	Expect(err).ToNot(HaveOccurred(), "there should be no error creating the mirror ConfigMap")
+}
+
+func SetupOCIMirroringForOrg(ctx context.Context, adminClient client.Client, orgName string) {
+	CreateMirrorConfigMap(ctx, adminClient, orgName)
+	patchOrgConfigMapRef(ctx, adminClient, orgName, "oci-replication-config")
+}
+
+func TeardownOCIMirroringForOrg(ctx context.Context, adminClient client.Client, orgName string) {
+	patchOrgConfigMapRef(ctx, adminClient, orgName, "")
+}
+
+func patchOrgConfigMapRef(ctx context.Context, adminClient client.Client, orgName, ref string) {
+	org := &greenhousev1alpha1.Organization{}
+	Expect(adminClient.Get(ctx, client.ObjectKey{Name: orgName}, org)).To(Succeed())
+	patch := client.MergeFrom(org.DeepCopy())
+	org.Spec.ConfigMapRef = ref
+	Expect(adminClient.Patch(ctx, org, patch)).To(Succeed())
+}
